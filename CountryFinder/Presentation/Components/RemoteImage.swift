a40a592d1ec3d87080c9b043b897cf0564c80f50
Mustafa Abozaina <mabozaina@airnxt.ai>
2025-10-08 22:25:42 +0300

    Refactor RemoteImage to expose underlying Image for flexibility

--- CountryFinder/Presentation/Components/RemoteImage.swift
+++ CountryFinder/Presentation/Components/RemoteImage.swift
@@ -8,40 +8,40 @@
 import SwiftUI
 import UIKit
 
-struct RemoteImage: View {
+struct RemoteImage<Content: View>: View {
     let url: URL?
     let placeholder: String
     let isSystemPlaceholder: Bool
+    let content: (Image) -> Content
     
     @State private var uiImage: UIImage? = nil
     @State private var isLoading: Bool = false
     
-    init(url: URL?, placeholder: String = "", isSystemPlaceholder: Bool = true) {
+    init(url: URL?,
+         placeholder: String = "",
+         isSystemPlaceholder: Bool = true,
+         @ViewBuilder content: @escaping (Image) -> Content = { $0.resizable() }) {
         self.url = url
         self.placeholder = placeholder
         self.isSystemPlaceholder = isSystemPlaceholder
+        self.content = content
     }
-
+        
     var body: some View {
         Group {
             if let uiImage {
-                Image(uiImage: uiImage)
-                    .resizable()
-                    .scaledToFill()
+                content(Image(uiImage: uiImage))
             } else if isLoading {
                 ProgressView()
             } else {
-                (isSystemPlaceholder
-                 ? Image(systemName: placeholder)
-                 : Image(placeholder))
-                .resizable()
-                .scaledToFit()
-                .font(.system(size: 56, weight: .regular))
-                .foregroundStyle(.secondary)
+                if isSystemPlaceholder {
+                    content(Image(systemName: placeholder))
+                } else {
+                    content(Image(placeholder))
+                }
             }
         }
-        .task {
-            load()
+        .task { load()
         }
     }
 
